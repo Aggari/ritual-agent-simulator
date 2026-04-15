3fd0c0a07c012cef426eae6b4081da649d013ec4
nana <86152022+Aggari@users.noreply.github.com>
2026-04-15 16:20:25 +0900

    Create layout.js

---
+++ app/layout.js
@@ -0,0 +1,12 @@
+export const metadata = {
+  title: "Ritual Agent Simulator",
+  description: "Created by badang",
+};
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <body style={{ margin: 0 }}>{children}</body>
+    </html>
+  );
+}
